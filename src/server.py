68396412ccb5dfc6a2a4c05ee6d80ff80f11600f
NickTooth96 <nicktoothaker@gmail.com>
2023-10-03 15:56:17 -0500

    More work

--- src/server.py
+++ src/server.py
@@ -1,7 +1,9 @@
 import datetime
+import os
+import shutil
 import toml
 
-
+ROOT_DIR = os.path.dirname(os.path.abspath('minecraft_pseudoserver.py'))
 def create(name):
     path = 'stand_in'
     with open('servers.toml', 'r') as f:
@@ -13,11 +15,23 @@ def create(name):
                 config['servers'][f'servers.{name}']['path'] = path
                 config['servers'][f'servers.{name}']['last_update'] = datetime.datetime.timestamp()
 
-def update_remote(name):
-    print(name)
+def update_remote(name):   
+    
     with open('servers.toml', 'r') as f:
         config = toml.load(f)
+        name = config[name]['name']
+        date = datetime.datetime.now()
+        path = config[name]['path']
+    copy(name,path)
+    os.system('git add -u')
+    os.system('git status')
+
 
 def update_local(name):
     print(name)
-        
+
+def copy(name,src_path):
+    dst = os.path.join(ROOT_DIR,'backups',name)
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    shutil.copytree(src_path, dst)        
